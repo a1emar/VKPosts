77b4cab21a4ec59e26aeecbbde8c8ca3750cba12
AlexeYeS <m0use@mail.ru>
2022-09-11 16:23:54 +0300

    Initial commit

---
+++ src/main/kotlin/ru/netology/Main.kt
@@ -0,0 +1,11 @@
+package ru.netology
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    class Post(id: Int, owner_id: Int, from_id: Int, taxt: String, friends_only: Boolean, comments: Objects, likes: Objects, ) {
+
+    }
+    println("Hello World!")
+    println("Program arguments: ${args.joinToString()}")
+}
